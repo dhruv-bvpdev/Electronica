7b965ba0a34fe83e00848dd3e63d975ed852bed4
Dhruv Gursahani <dhruv.gursahani@outlook.com>
2021-12-20 20:12:11 +0530

    created product,user,order model, added data to DB

---
+++ backend/data/users.js
@@ -0,0 +1,22 @@
+import bcrypt from "bcrypt";
+
+const users = [
+  {
+    name: "Admin User",
+    email: "admin@example.com",
+    password: bcrypt.hashSync("!V4p6wCUgo9%78", 10),
+    isAdmin: true,
+  },
+  {
+    name: "John Doe",
+    email: "john@example.com",
+    password: bcrypt.hashSync("b878gud3P3Y*5@", 10),
+  },
+  {
+    name: "Jane Doe",
+    email: "jane@example.com",
+    password: bcrypt.hashSync("88*P$UEEjbnyLCy", 10),
+  },
+];
+
+export default users;
---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/connectDB.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API working fine");
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(
+    `Server up and running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  );
+});

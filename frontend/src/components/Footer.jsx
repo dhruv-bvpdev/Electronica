74077477c5e60873c13cb004e19d0eb72dbe3c05
Dhruv Gursahani <dhruv.gursahani@outlook.com>
2021-12-20 20:10:36 +0530

    updated footer component

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
 const Footer = () => {
+  const year = new Date().getFullYear();
+
   return (
     <footer style={{ "background-color": "black", color: "#ffff" }}>
       <Container>
         <Row>
-          <Col className="text-center mt-3">Copyright &copy; 2022</Col>
+          <Col className="text-center mt-3">Copyright &copy; {year}</Col>
         </Row>
         <Row>
           <Col className="text-center my-3">Electronica</Col>

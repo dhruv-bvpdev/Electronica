bf426b8e34c1a353b65f01ef679b5be6de57e0e2
Dhruv Gursahani <dhruv.gursahani@outlook.com>
2021-12-28 12:15:52 +0530

    product review functionality

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,12 +5,14 @@ import {
   getProductById,
   getProducts,
   updateProduct,
+  createProductReview,
 } from "../controllers/productController.js";
 const router = express.Router();
 
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
 router
   .route("/:id")
   .get(getProductById)
